d796f13c57c14344d435146769610914bd93042c
garronej <joseph.garrone@ensimag.grenoble-inp.fr>
2017-03-06 14:50:33 +0000

    fix export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,19 @@
 export * from "./Modem";
-export * from "./SerialPortExt";
 export * from "at-messages-parser";
 export { Contact } from "./CardStorage";
+
+import  * as SerialPortExt from "./SerialPortExt";
+import * as AtStack from "./AtStack";
+import * as CardStorage from "./CardStorage";
+import * as CardLockFacility from "./CardLockFacility";
+import * as SmsStack from "./SmsStack";
+import * as SystemState from "./SystemState";
+
+export let dev= {
+    SerialPortExt,
+    AtStack,
+    CardStorage,
+    CardLockFacility,
+    SmsStack,
+    SystemState
+};
