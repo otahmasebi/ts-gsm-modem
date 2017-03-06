6b9dbab49c15b4cf2adb129b6e28952f7e1269b3
garronej <joseph.garrone@ensimag.grenoble-inp.fr>
2017-03-06 16:45:18 +0000

    fix stupid exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,19 +1,8 @@
 export * from "./Modem";
 export * from "at-messages-parser";
 export { Contact } from "./CardStorage";
+export { Message, StatusReport } from "./SmsStack";
 
-import  * as SerialPortExt from "./SerialPortExt";
-import * as AtStack from "./AtStack";
-import * as CardStorage from "./CardStorage";
-import * as CardLockFacility from "./CardLockFacility";
-import * as SmsStack from "./SmsStack";
-import * as SystemState from "./SystemState";
 
-export let dev= {
-    SerialPortExt,
-    AtStack,
-    CardStorage,
-    CardLockFacility,
-    SmsStack,
-    SystemState
-};
+
+
